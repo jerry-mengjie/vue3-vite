2e95fc47f4578ed8a62896886fea2f757806065c
ganmengjie <ganmengjie@qtrade.com.cn>
2022-01-19 16:18:44 +0800

    feat: 添加vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "Home",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "About",
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// 前置路由
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title as string;
+  next();
+});
+
+export default router;
