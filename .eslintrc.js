3fc2cf619b98a4af398294dfe5c887c73866a5e2
ganmengjie <ganmengjie@qtrade.com.cn>
2022-02-22 13:49:02 +0800

    fix: 添加eslint max限制

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-lines': ['error', 500], // 此规则为每个文件强制执行最多行数，以帮助维护和降低复杂性。
+    'max-depth': ['error', 4], // 此规则强制执行嵌套块的最大深度，以降低代码复杂度。
+    'max-nested-callbacks': ['error', 3], //此规则强制执行最大深度，可以嵌套回调以提高代码清晰度。
   },
   overrides: [
     {
